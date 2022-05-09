5e682bfe21c0c05721be6fee08d35d9ed81bff94
skrux <sc20sj@leeds.ac.uk>
2022-05-10 00:38:24 +0800

    game algorithm

--- main.c
+++ main.c
@@ -1,6 +1,16 @@
 #include <stdio.h>
-
+#include <stdlib.h>
+#include <string.h>
+#include "utility.h"
 int main() {
-    printf("Hello, World!\n");
-    return 0;
+    pBoard pBoard1 = malloc(sizeof (Board));
+    loadFile("game.txt", pBoard1);
+    printf("%d+%d\n", pBoard1->row, pBoard1->col);
+    for(int i=0; i<pBoard1->row; i++){
+        for(int j=0; j<pBoard1->col; j++){
+            printf("%c", pBoard1->boardArr[i][j]);
+        }
+        printf("\n");
+    }
+    runGame(pBoard1);
 }
